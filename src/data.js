a8833345a186cf231a9685682c8e1b112a9f4174
Aelita <mustafausakli_@hotmail.com>
2022-06-10 20:40:18 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,30 @@
+const questions = [
+    {
+        id: 1,
+        title: "What is Lorem Ipsum?",
+        info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+        id: 2,
+        title: "Why do we use it?",
+        info: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. "
+    },
+    {
+        id: 3,
+        title: "Where does it come from?",
+        info: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."
+    },
+    {
+        id: 4,
+        title: "Where can I get some?",
+        info: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
+    },
+    {
+        id: 5,
+        title: "Lorem Ipsum?",
+        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec rutrum nibh. Proin at ligula vel dui dapibus vestibulum. Sed tincidunt lacus et dolor dignissim eleifend. Aenean sed mattis lorem. Quisque congue tincidunt sem ut posuere. Donec et egestas diam, vel varius eros. Sed magna mauris, consectetur sed ex eu, bibendum tincidunt dui."
+
+    }
+]
+
+export default questions
